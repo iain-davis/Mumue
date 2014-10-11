bc6ce711b374e41fd9b44990fc006bb9f1356192
Iain E. Davis <iain@ruhlendavis.org>
2014-10-11 13:44:00 -0500

    add basic support for separating sections.

--- src/main/java/org/ruhlendavis/meta/importer/SeparateSectionsStage.java
+++ src/main/java/org/ruhlendavis/meta/importer/SeparateSectionsStage.java
@@ -1,8 +1,15 @@
 package org.ruhlendavis.meta.importer;
 
+import java.util.List;
+
 public class SeparateSectionsStage extends ImporterStage {
     @Override
     public void run(ImportBucket bucket) {
-
+        List<String> sourceLines = bucket.getSourceLines();
+        if (sourceLines.size() == 0 || !"***Foxen5 TinyMUCK DUMP Format***".equals(sourceLines.get(0))) {
+            bucket.setFailed(true);
+            return;
+        }
+        bucket.setDatabaseItemCount(Long.parseLong(sourceLines.get(1)));
     }
 }
--- src/test/java/org/ruhlendavis/meta/importer/SeparateSectionsStageTest.java
+++ src/test/java/org/ruhlendavis/meta/importer/SeparateSectionsStageTest.java
@@ -1,12 +1,63 @@
 package org.ruhlendavis.meta.importer;
 
+import com.google.common.io.Resources;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SeparateSectionsStageTest {
     SeparateSectionsStage stage = new SeparateSectionsStage();
 
     @Test
-    public void a() {
+    public void runHandlesNoLines() {
+        ImportBucket bucket = new ImportBucket();
+        bucket.setSourceLines(new ArrayList<>());
+        stage.run(bucket);
+        assertTrue(bucket.isFailed());
+    }
 
+    @Test
+    public void runConfirmsDatabaseVersion() throws URISyntaxException {
+        ImportBucket bucket = new ImportBucket();
+        List<String> lines = new ArrayList<>();
+        lines.add(RandomStringUtils.randomAlphabetic(25));
+        bucket.setSourceLines(lines);
+        stage.run(bucket);
+        assertTrue(bucket.isFailed());
+    }
+
+    @Test
+    public void runRetrievesDatabaseItemCount() throws URISyntaxException {
+        ImportBucket bucket = new ImportBucket();
+        List<String> lines = new ArrayList<>();
+        lines.add("***Foxen5 TinyMUCK DUMP Format***");
+        String count = RandomStringUtils.randomNumeric(5);
+        lines.add(count);
+        bucket.setSourceLines(lines);
+        stage.run(bucket);
+        assertEquals(Long.parseLong(count), bucket.getDatabaseItemCount(), 0);
     }
 }
+//    @Test
+//    public void runRejectsCompressedFile() throws URISyntaxException {
+//        ImportBucket bucket = new ImportBucket();
+//        URI uri = Resources.getResource("org/ruhlendavis/meta/importer/SeparateSectionsStageTestCompressedInput.db").toURI();
+//        bucket.setFile(uri.getPath());
+//        stage.run(bucket);
+//        assertEquals(0, bucket.getComponentLines().size());
+//    }
+//    @Test
+//    public void runHandlesTestInputFile() throws URISyntaxException {
+//        ImportBucket bucket = new ImportBucket();
+//        URI uri = Resources.getResource("org/ruhlendavis/meta/importer/SeparateSectionsStageTestInput.db").toURI();
+//        bucket.setFile(uri.getPath());
+//        stage.run(bucket);
+//        assertEquals(300, bucket.getParameterLines().size());
+//        assertEquals(7, bucket.getComponentLines().size());
