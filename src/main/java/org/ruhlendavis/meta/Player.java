265eecfd5b5ac1023f1fec654abea1de9cc4d783
Iain E. Davis <iain@ruhlendavis.org>
2014-10-15 07:58:03 -0500

    Added fields to Player class.

--- src/main/java/org/ruhlendavis/meta/Player.java
+++ src/main/java/org/ruhlendavis/meta/Player.java
@@ -1,4 +1,30 @@
 package org.ruhlendavis.meta;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    String name = "";
+    String password = "";
+    List<Character> characters = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Character> getCharacters() {
+        return characters;
+    }
 }
