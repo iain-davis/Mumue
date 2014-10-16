a44554005db96b900642b5fc5d5bca4d09e05116
Iain E. Davis <iain@ruhlendavis.org>
2014-10-16 08:03:59 -0500

    MetaMain reads configuration path from command line.

--- src/main/java/org/ruhlendavis/meta/MetaMain.java
+++ src/main/java/org/ruhlendavis/meta/MetaMain.java
@@ -1,4 +1,23 @@
 package org.ruhlendavis.meta;
 
+import java.io.InputStream;
+import java.util.Properties;
+import org.apache.commons.lang3.StringUtils;
+
 public class MetaMain {
+    private String configurationPath = "configuration.properties";
+
+    public void run(String[] arguments) {
+        if (arguments.length == 1 && StringUtils.isNotBlank(arguments[0])) {
+            configurationPath = arguments[0];
+        }
+    }
+
+    public String getConfigurationPath() {
+        return configurationPath;
+    }
+
+    public void setConfigurationPath(String configurationPath) {
+        this.configurationPath = configurationPath;
+    }
 }
--- src/test/java/org/ruhlendavis/meta/MetaMainTest.java
+++ src/test/java/org/ruhlendavis/meta/MetaMainTest.java
@@ -1,4 +1,28 @@
 package org.ruhlendavis.meta;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStream;
+import java.util.Properties;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
 public class MetaMainTest {
+    private MetaMain main = new MetaMain();
+
+    @Test
+    public void runParsesRetrievesConfigurationFromCommandLine() {
+        String path = RandomStringUtils.randomAlphabetic(13);
+        main.run(new String[]{path});
+        assertEquals(path, main.getConfigurationPath());
+    }
+
+    @Test
+    public void runSetsDefaultPath() {
+        String path = "configuration.properties";
+        main.run(new String[]{});
+        assertEquals(path, main.getConfigurationPath());
+    }
 }
