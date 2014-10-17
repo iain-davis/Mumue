4ae6ee315ac28c9632deec98f41950354db535ac
Iain E. Davis <iain@ruhlendavis.org>
2014-10-16 19:45:41 -0500

    Implement load method within Configuration class

--- src/main/java/org/ruhlendavis/meta/Configuration.java
+++ src/main/java/org/ruhlendavis/meta/Configuration.java
@@ -1,9 +1,21 @@
 package org.ruhlendavis.meta;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
+import org.apache.commons.io.FileUtils;
 
 public class Configuration {
     private Properties properties = new Properties();
+
+    public void load(String configurationPath) {
+        try {
+            InputStream input = FileUtils.openInputStream(FileUtils.getFile(configurationPath));
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public int getPort() {
         return Integer.parseInt(properties.getProperty("port"));
--- src/test/java/org/ruhlendavis/meta/ConfigurationTest.java
+++ src/test/java/org/ruhlendavis/meta/ConfigurationTest.java
@@ -1,25 +1,31 @@
 package org.ruhlendavis.meta;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
+import com.google.common.io.Resources;
+import java.net.URISyntaxException;
 import java.util.Properties;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.Before;
 import org.junit.Test;
 
 public class ConfigurationTest {
-    private Properties properties = new Properties();
-    private Configuration configuration = new Configuration();
-
-    @Before
-    public void beforeEach() {
+    @Test
+    public void getPortReturnsPortProperty() {
+        Configuration configuration = new Configuration();
+        Properties properties = new Properties();
         configuration.setProperties(properties);
+        String port = RandomStringUtils.randomNumeric(4);
+        properties.setProperty("port", port);
+        
+        assertEquals(Integer.parseInt(port), configuration.getPort());
     }
 
     @Test
-    public void getPortReturnsPortProperty() {
-        String port = RandomStringUtils.randomNumeric(4);
-        properties.setProperty("port", port);
-        assertEquals(Integer.parseInt(port), configuration.getPort());
+    public void loadLoadsConfiguration() throws URISyntaxException {
+        Configuration configuration = new Configuration();
+        String path = Resources.getResource("org/ruhlendavis/meta/configuration.properties").toURI().getPath();
+        configuration.load(path);
+        assertEquals(9999, configuration.getPort());
     }
 }
