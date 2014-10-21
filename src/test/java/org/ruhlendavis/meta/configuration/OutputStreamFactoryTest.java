645a06b42c9d288eeeb9f73b31cc3c0517d766af
Iain E. Davis <iain@ruhlendavis.org>
2014-10-21 18:16:34 -0500

    Clean up after test.

--- src/test/java/org/ruhlendavis/meta/configuration/OutputStreamFactoryTest.java
+++ src/test/java/org/ruhlendavis/meta/configuration/OutputStreamFactoryTest.java
@@ -2,8 +2,10 @@ package org.ruhlendavis.meta.configuration;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.h2.store.fs.FileUtils;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -13,9 +15,11 @@ public class OutputStreamFactoryTest {
     @InjectMocks private OutputStreamFactory factory = new OutputStreamFactory();
 
     @Test
-    public void createOutputStreamCreatesStream() {
+    public void createOutputStreamCreatesStream() throws IOException {
         String path = RandomStringUtils.randomAlphabetic(13);
         OutputStream output = factory.createOutputStream(path);
         assertNotNull(output);
+        output.close();
+        FileUtils.delete(path);
     }
 }
