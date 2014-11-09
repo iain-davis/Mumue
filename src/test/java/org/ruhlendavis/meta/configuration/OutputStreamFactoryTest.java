a5aba24f4794698e59a2c109ffebfcdedf2cb4b3
Iain E. Davis <iain@ruhlendavis.org>
2014-11-09 09:13:26 -0600

    Remove unneeded code.

--- src/test/java/org/ruhlendavis/meta/configuration/OutputStreamFactoryTest.java
+++ src/test/java/org/ruhlendavis/meta/configuration/OutputStreamFactoryTest.java
@@ -4,15 +4,13 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.io.OutputStream;
+
 import org.apache.commons.lang3.RandomStringUtils;
 import org.h2.store.fs.FileUtils;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 
 public class OutputStreamFactoryTest {
-    @Mock private FileFactory fileFactory = new FileFactory();
-    @InjectMocks private OutputStreamFactory factory = new OutputStreamFactory();
+    private OutputStreamFactory factory = new OutputStreamFactory();
 
     @Test
     public void createOutputStreamCreatesStream() throws IOException {
