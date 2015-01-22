16c9f96cbb84229f9ff7b7ed7b25ebfc99db788a
Iain E. Davis <iain@ruhlendavis.org>
2015-01-22 08:45:40 -0600

    Sketched in the connection run method.

--- src/main/java/org/ruhlendavis/meta/connection/Connection.java
+++ src/main/java/org/ruhlendavis/meta/connection/Connection.java
@@ -2,10 +2,27 @@ package org.ruhlendavis.meta.connection;
 
 import java.util.Vector;
 
-public abstract class Connection extends CleanCloser implements Runnable {
+import org.ruhlendavis.meta.configuration.Configuration;
+
+public class Connection extends CleanCloser implements Runnable {
+    private Configuration configuration;
     private Vector<String> linesReceived = new Vector<>();
 
     public Vector<String> getLinesReceived() {
         return linesReceived;
+    }
+
+    @Override
+    public void run() {
+        do {
+//            for (String line : linesReceived) {
+//
+//            }
+        } while (!configuration.isTest());
+    }
+
+    public Connection withConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+        return this;
     }
 }
