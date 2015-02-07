401b549b64759de677b7e930fecfbf16a6d1cbec
Iain E. Davis <iain@ruhlendavis.org>
2015-02-07 13:00:56 -0600

    clean up unneeded tests.

--- src/test/java/org/ruhlendavis/meta/acceptance/TestConstants.java
+++ src/test/java/org/ruhlendavis/meta/acceptance/TestConstants.java
@@ -6,6 +6,4 @@ public interface TestConstants {
     String MEMORY_DATABASE = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
     String QUERY_PURGE_DATABASE = "DROP ALL OBJECTS";
     String TEST_CONFIGURATION_FILE_PATH = "org/ruhlendavis/meta/configuration/startup/" + ConfigurationDefaults.CONFIGURATION_PATH;
-    String ACCEPTANCE_STARTUP_CONFIGURATION_PATH = "org/ruhlendavis/meta/acceptance/acceptance-startup-configuration.properties";
-    String ACCEPTANCE_DATABASE_PATH = "./acceptance.h2.db";
 }
