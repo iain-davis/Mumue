f4bcd92e5a2dbebb4cc3914d67804d69f7060624
Iain E. Davis <iain@ruhlendavis.org>
2015-02-18 22:09:28 -0600

    Clean up unused import.

--- src/main/java/org/ruhlendavis/meta/components/ComponentDao.java
+++ src/main/java/org/ruhlendavis/meta/components/ComponentDao.java
@@ -6,12 +6,12 @@ import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 
-import org.ruhlendavis.meta.components.Component;
 import org.ruhlendavis.meta.database.QueryRunnerProvider;
 import org.ruhlendavis.meta.importer.GlobalConstants;
 
 public class ComponentDao {
     private static final String GET_QUERY = "select * from components where id = ?";
+
     public Component getComponent(long id) {
         QueryRunner database = QueryRunnerProvider.get();
         ResultSetHandler<Component> resultSetHandler = new BeanHandler<>(Component.class);
