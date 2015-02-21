835652c33a02326fe2434c7c1e8d2d4f2d5054fa
Iain E. Davis <iain@ruhlendavis.org>
2015-02-21 11:42:32 -0600

    Fix thread errors generated by ConnectionFactoryTest

--- src/main/java/org/ruhlendavis/mumue/connection/ConnectionFactory.java
+++ src/main/java/org/ruhlendavis/mumue/connection/ConnectionFactory.java
@@ -5,9 +5,10 @@ import java.net.Socket;
 import org.ruhlendavis.mumue.configuration.Configuration;
 
 public class ConnectionFactory {
+    private ConnectionInitializer connectionInitializer = new ConnectionInitializer();
+
     public Connection create(Socket socket, Configuration configuration) {
         Connection connection = new Connection();
-        ConnectionInitializer connectionInitializer = new ConnectionInitializer();
         connectionInitializer.initialize(socket, connection, configuration);
         return connection;
     }
--- src/test/java/org/ruhlendavis/mumue/connection/ConnectionFactoryTest.java
+++ src/test/java/org/ruhlendavis/mumue/connection/ConnectionFactoryTest.java
@@ -2,9 +2,15 @@ package org.ruhlendavis.mumue.connection;
 
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+
+import java.net.Socket;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -12,11 +18,20 @@ import org.ruhlendavis.mumue.configuration.Configuration;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ConnectionFactoryTest {
-    private final ConnectionFactory connectionFactory = new ConnectionFactory();
     @Mock Configuration configuration;
+    @Mock Socket socket;
+    @Mock ConnectionInitializer connectionInitializer;
+    @InjectMocks ConnectionFactory connectionFactory;
 
     @Test
     public void createReturnsConnection() {
-        assertThat(connectionFactory.create(null, configuration), instanceOf(Connection.class));
+        assertThat(connectionFactory.create(socket, configuration), instanceOf(Connection.class));
+    }
+
+    @Test
+    public void createInitializesConnection() {
+        connectionFactory.create(socket, configuration);
+
+        verify(connectionInitializer).initialize(eq(socket), any(Connection.class), eq(configuration));
     }
 }
