9b1bb495c2cf32d44681217e3de06d18c493e0a4
Iain E. Davis <iain@ruhlendavis.org>
2015-03-06 20:59:29 -0600

    Implemented pose.

--- src/main/java/org/ruhlendavis/mumue/interpreter/commands/CommandPose.java
+++ src/main/java/org/ruhlendavis/mumue/interpreter/commands/CommandPose.java
@@ -1,9 +1,20 @@
 package org.ruhlendavis.mumue.interpreter.commands;
 
 import org.ruhlendavis.mumue.components.character.GameCharacter;
+import org.ruhlendavis.mumue.connection.Connection;
+import org.ruhlendavis.mumue.connection.ConnectionManager;
 
 public class CommandPose implements Command {
+    private ConnectionManager connectionManager = new ConnectionManager();
+
     @Override
     public void execute(GameCharacter character, String command, String arguments) {
+        String s = command.equals(";") ? "" : " ";
+        String output = character.getName() + s + arguments + "\\r\\n";
+        for (Connection connection : connectionManager.getConnections()) {
+            if (connection.getCharacter().getLocationId() == character.getLocationId()) {
+                connection.getOutputQueue().push(output);
+            }
+        }
     }
 }
--- src/test/java/org/ruhlendavis/mumue/interpreter/commands/CommandPoseTest.java
+++ src/test/java/org/ruhlendavis/mumue/interpreter/commands/CommandPoseTest.java
@@ -1,5 +1,87 @@
 package org.ruhlendavis.mumue.interpreter.commands;
 
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Vector;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import org.ruhlendavis.mumue.components.character.CharacterBuilder;
+import org.ruhlendavis.mumue.components.character.GameCharacter;
+import org.ruhlendavis.mumue.connection.Connection;
+import org.ruhlendavis.mumue.connection.ConnectionManager;
+
 public class CommandPoseTest {
-    private CommandPose command = new CommandPose();
+    @Rule public MockitoRule mockito = MockitoJUnit.rule();
+    @Mock ConnectionManager connectionManager;
+    @InjectMocks CommandPose commandPose;
+    private final GameCharacter poser = new CharacterBuilder().withLocationId(RandomUtils.nextLong(100, 200)).build();
+    private final Connection posingConnection = new Connection().withCharacter(poser);
+    private final Vector<Connection> connections = new Vector<>();
+
+    @Before
+    public void beforeEach() {
+        connections.add(posingConnection);
+        when(connectionManager.getConnections()).thenReturn(connections);
+    }
+
+    @Test
+    public void poseSeenByPoserWithSpace() {
+        poser.setName(RandomStringUtils.randomAlphabetic(17));
+        String message = RandomStringUtils.randomAlphabetic(35);
+        String expected = poser.getName() + " " + message + "\\r\\n";
+
+        commandPose.execute(poser, ":", message);
+
+        assertThat(posingConnection.getOutputQueue(), hasItem(expected));
+    }
+
+    @Test
+    public void poseSeenByPoserWithoutSpace() {
+        poser.setName(RandomStringUtils.randomAlphabetic(17));
+        String message = RandomStringUtils.randomAlphabetic(35);
+        String expected = poser.getName() + message + "\\r\\n";
+
+        commandPose.execute(poser, ";", message);
+
+        assertThat(posingConnection.getOutputQueue(), hasItem(expected));
+    }
+
+    @Test
+    public void poseSeenByOtherCharacterInRoom() {
+        Connection inRoomConnection = new Connection().withCharacter(new CharacterBuilder().withLocationId(poser.getLocationId()).build());
+        poser.setName(RandomStringUtils.randomAlphabetic(17));
+        String message = RandomStringUtils.randomAlphabetic(35);
+        String expected = poser.getName() + " " + message + "\\r\\n";
+        connections.add(inRoomConnection);
+
+        commandPose.execute(poser, ":", message);
+
+        assertThat(inRoomConnection.getOutputQueue(), hasItem(expected));
+    }
+
+    @Test
+    public void poseNotSeenByCharacterInOtherRoom() {
+        Connection otherRoomConnection = new Connection().withCharacter(new CharacterBuilder().withLocationId(RandomUtils.nextLong(500, 600)).build());
+        poser.setName(RandomStringUtils.randomAlphabetic(17));
+        String message = RandomStringUtils.randomAlphabetic(35);
+        connections.add(otherRoomConnection);
+
+        commandPose.execute(poser, ":", message);
+
+        assertThat(otherRoomConnection.getOutputQueue().size(), equalTo(0));
+    }
 }
