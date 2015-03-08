4341e145753e6d57413b017aa2834c646321088b
Iain E. Davis <iain@ruhlendavis.org>
2015-03-08 13:36:40 -0500

    update SpaceDao to use DatabaseAccessor.

--- src/main/java/org/ruhlendavis/mumue/components/space/SpaceDao.java
+++ src/main/java/org/ruhlendavis/mumue/components/space/SpaceDao.java
@@ -1,27 +1,21 @@
 package org.ruhlendavis.mumue.components.space;
 
-import java.sql.SQLException;
-
-import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 
-import org.ruhlendavis.mumue.database.QueryRunnerProvider;
+import org.ruhlendavis.mumue.database.DatabaseAccessor;
+import org.ruhlendavis.mumue.database.DatabaseAccessorProvider;
 
 public class SpaceDao {
     private static final String GET_QUERY = "select * from spaces where id = ?";
 
     public Space getSpace(long spaceId) {
-        QueryRunner database = QueryRunnerProvider.get();
+        DatabaseAccessor database = DatabaseAccessorProvider.get();
         ResultSetHandler<Space> resultSetHandler = new BeanHandler<>(Space.class, new SpaceRowProcessor());
-        try {
-            Space space = database.query(GET_QUERY, resultSetHandler, spaceId);
-            if (space == null) {
-                return new Space();
-            }
-            return space;
-        } catch (SQLException exception) {
-            throw new RuntimeException(exception);
+        Space space = database.query(GET_QUERY, resultSetHandler, spaceId);
+        if (space == null) {
+            return new Space();
         }
+        return space;
     }
 }
