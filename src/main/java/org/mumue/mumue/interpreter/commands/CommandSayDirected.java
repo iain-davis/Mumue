8cdfac4851b58ae0f7e04dad2c73f3f2eaaf8d68
Iain E. Davis <iain@ruhlendavis.org>
2016-01-16 17:00:38 -0600

    Update tests so they run against injected class.

--- src/main/java/org/mumue/mumue/interpreter/commands/CommandSayDirected.java
+++ src/main/java/org/mumue/mumue/interpreter/commands/CommandSayDirected.java
@@ -1,17 +1,24 @@
 package org.mumue.mumue.interpreter.commands;
 
+import javax.inject.Inject;
+
 import org.apache.commons.lang3.StringUtils;
 import org.mumue.mumue.components.character.GameCharacter;
 import org.mumue.mumue.configuration.ApplicationConfiguration;
 import org.mumue.mumue.connection.Connection;
 import org.mumue.mumue.connection.ConnectionManager;
-import org.mumue.mumue.importer.GlobalConstants;
 import org.mumue.mumue.text.TextMaker;
 import org.mumue.mumue.text.TextName;
 
 public class CommandSayDirected implements Command {
-    private ConnectionManager connectionManager = new ConnectionManager();
-    private TextMaker textMaker = new TextMaker();
+    private final ConnectionManager connectionManager;
+    private final TextMaker textMaker;
+
+    @Inject
+    public CommandSayDirected(ConnectionManager connectionManager, TextMaker textMaker) {
+        this.connectionManager = connectionManager;
+        this.textMaker = textMaker;
+    }
 
     @Override
     public void execute(Connection connection, String command, String arguments, ApplicationConfiguration configuration) {
--- src/test/java/org/mumue/mumue/interpreter/commands/CommandSayDirectedTest.java
+++ src/test/java/org/mumue/mumue/interpreter/commands/CommandSayDirectedTest.java
@@ -3,80 +3,104 @@ package org.mumue.mumue.interpreter.commands;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-import java.util.Vector;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
 import org.mumue.mumue.components.character.CharacterBuilder;
 import org.mumue.mumue.components.character.GameCharacter;
 import org.mumue.mumue.configuration.ApplicationConfiguration;
 import org.mumue.mumue.connection.Connection;
 import org.mumue.mumue.connection.ConnectionManager;
+import org.mumue.mumue.importer.GlobalConstants;
 import org.mumue.mumue.player.Player;
 import org.mumue.mumue.player.PlayerBuilder;
+import org.mumue.mumue.testobjectbuilder.TestObjectBuilder;
 import org.mumue.mumue.text.TextMaker;
 import org.mumue.mumue.text.TextName;
 
 public class CommandSayDirectedTest {
-    @Rule public MockitoRule mockito = MockitoJUnit.rule();
-    @Mock TextMaker textMaker;
-    @Mock ConnectionManager connectionManager;
-    @Mock
-    ApplicationConfiguration configuration;
-    @InjectMocks CommandSayDirected command;
-
+    private final TextMaker textMaker = mock(TextMaker.class);
+    private final ConnectionManager connectionManager = new ConnectionManager();
+    private final ApplicationConfiguration configuration = TestObjectBuilder.configuration();
     private final String locale = RandomStringUtils.randomAlphabetic(16);
-    private final Vector<Connection> connections = new Vector<>();
     private final Player player = new PlayerBuilder().withLocale(locale).build();
-    private final GameCharacter sayer = new CharacterBuilder().withName(RandomStringUtils.randomAlphabetic(7))
-            .withLocationId(RandomUtils.nextLong(100, 200)).build();
-    private final GameCharacter target = new CharacterBuilder().withName(RandomStringUtils.randomAlphabetic(8).toUpperCase())
-            .withLocationId(sayer.getLocationId()).build();
-    private final Connection sayerConnection = new Connection(configuration).withPlayer(player).withCharacter(sayer);
-    private final Connection targetConnection = new Connection(configuration).withCharacter(target);
 
-    @Before
-    public void beforeEach() {
-        connections.add(sayerConnection);
-        connections.add(targetConnection);
-        when(connectionManager.getConnections()).thenReturn(connections);
-    }
+    private final CommandSayDirected command = new CommandSayDirected(connectionManager, textMaker);
 
     @Test
     public void messageSeenBySayer() {
         String message = RandomStringUtils.randomAlphabetic(17);
+        String characterName = RandomStringUtils.randomAlphabetic(7);
+        String targetName = RandomStringUtils.randomAlphabetic(8);
+        long locationId = RandomUtils.nextLong(100, 200);
+        Connection sayerConnection = connection(null, character(characterName, locationId));
+        connectionManager.add(sayerConnection);
+        connectionManager.add(connection(null, character(targetName, locationId)));
 
-        command.execute(sayerConnection, "`", target.getName() + " " + message, configuration);
-        String expected = sayer.getName() + " says to " + target.getName() + ", \"" + message + "\"\\r\\n";
+        command.execute(sayerConnection, "`", targetName + " " + message, configuration);
+
+        String expected = characterName + " says to " + targetName + ", \"" + message + "\"" + GlobalConstants.NEW_LINE;
+        assertThat(sayerConnection.getOutputQueue(), hasItem(expected));
+    }
+
+    @Test
+    public void messageWithSpace() {
+        String message = RandomStringUtils.randomAlphabetic(17) + " " + RandomStringUtils.randomAlphabetic(17);
+        String characterName = RandomStringUtils.randomAlphabetic(7);
+        String targetName = RandomStringUtils.randomAlphabetic(8);
+        long locationId = RandomUtils.nextLong(100, 200);
+        Connection sayerConnection = connection(null, character(characterName, locationId));
+        connectionManager.add(sayerConnection);
+        connectionManager.add(connection(null, character(targetName, locationId)));
+
+        command.execute(sayerConnection, "`", targetName + " " + message, configuration);
+
+        String expected = characterName + " says to " + targetName + ", \"" + message + "\"" + GlobalConstants.NEW_LINE;
         assertThat(sayerConnection.getOutputQueue(), hasItem(expected));
     }
 
     @Test
     public void targetMatchPartial() {
-        when(textMaker.getText(TextName.TargetBeingNotFound, locale)).thenReturn("");
+        String locale = RandomStringUtils.randomAlphabetic(16);
         String message = RandomStringUtils.randomAlphabetic(17);
+        String characterName = RandomStringUtils.randomAlphabetic(7);
+        String targetName = RandomStringUtils.randomAlphabetic(8);
+        long locationId = RandomUtils.nextLong(100, 200);
 
-        command.execute(sayerConnection, "`", target.getName().substring(0, RandomUtils.nextInt(3, 6)) + " " + message, configuration);
-        String expected = sayer.getName() + " says to " + target.getName() + ", \"" + message + "\"\\r\\n";
+        Player targetPlayer = new PlayerBuilder().withLocale(locale).build();
+        Connection sayerConnection = connection(null, character(characterName, locationId));
+        connectionManager.add(sayerConnection);
+        connectionManager.add(connection(targetPlayer, character(targetName, locationId)));
+
+        when(textMaker.getText(TextName.TargetBeingNotFound, locale)).thenReturn("");
+
+        command.execute(sayerConnection, "`", targetName.substring(0, RandomUtils.nextInt(3, 6)) + " " + message, configuration);
+
+        String expected = characterName + " says to " + targetName + ", \"" + message + "\"" + GlobalConstants.NEW_LINE;
         assertThat(sayerConnection.getOutputQueue(), hasItem(expected));
     }
 
     @Test
     public void targetMatchCaseInsensitive() {
-        when(textMaker.getText(TextName.TargetBeingNotFound, locale)).thenReturn("");
+        String locale = RandomStringUtils.randomAlphabetic(16);
         String message = RandomStringUtils.randomAlphabetic(17);
+        String characterName = RandomStringUtils.randomAlphabetic(7);
+        String targetName = RandomStringUtils.randomAlphabetic(8);
+        long locationId = RandomUtils.nextLong(100, 200);
 
-        command.execute(sayerConnection, "`", target.getName().toLowerCase() + " " + message, configuration);
-        String expected = sayer.getName() + " says to " + target.getName() + ", \"" + message + "\"\\r\\n";
+        Player targetPlayer = new PlayerBuilder().withLocale(locale).build();
+        Connection sayerConnection = connection(null, character(characterName, locationId));
+        connectionManager.add(sayerConnection);
+        connectionManager.add(connection(targetPlayer, character(targetName, locationId)));
+
+        when(textMaker.getText(TextName.TargetBeingNotFound, locale)).thenReturn("");
+
+        command.execute(sayerConnection, "`", targetName.toLowerCase() + " " + message, configuration);
+
+        String expected = characterName + " says to " + targetName + ", \"" + message + "\"" + GlobalConstants.NEW_LINE;
         assertThat(sayerConnection.getOutputQueue(), hasItem(expected));
     }
 
@@ -85,6 +109,10 @@ public class CommandSayDirectedTest {
         String responseMessage = RandomStringUtils.randomAlphabetic(18);
         String message = RandomStringUtils.randomAlphabetic(17);
         String otherTarget = RandomStringUtils.randomAlphabetic(8);
+        Player player = new PlayerBuilder().withLocale(locale).build();
+
+        Connection sayerConnection = connection(player, character("", 0));
+        connectionManager.add(sayerConnection);
 
         when(textMaker.getText(TextName.TargetBeingNotFound, locale)).thenReturn(responseMessage);
 
@@ -94,22 +122,14 @@ public class CommandSayDirectedTest {
     }
 
     @Test
-    public void messageWithSpace() {
-        String message = RandomStringUtils.randomAlphabetic(17) + " " + RandomStringUtils.randomAlphabetic(17);
-
-        command.execute(sayerConnection, "`", target.getName() + " " + message, configuration);
-
-        String expected = sayer.getName() + " says to " + target.getName() + ", \"" + message + "\"\\r\\n";
-        assertThat(sayerConnection.getOutputQueue(), hasItem(expected));
-    }
-
-    @Test
     public void noMessage() {
         String responseMessage = RandomStringUtils.randomAlphabetic(18);
+        Connection sayerConnection = connection(player, character("", 0));
+        connectionManager.add(sayerConnection);
 
         when(textMaker.getText(TextName.MissingSayText, locale)).thenReturn(responseMessage);
 
-        command.execute(sayerConnection, "`", target.getName(), configuration);
+        command.execute(sayerConnection, "`", "", configuration);
 
         assertThat(sayerConnection.getOutputQueue(), hasItem(responseMessage));
     }
@@ -117,46 +137,70 @@ public class CommandSayDirectedTest {
     @Test
     public void emptyMessage() {
         String responseMessage = RandomStringUtils.randomAlphabetic(18);
+        Connection sayerConnection = connection(player, character("", 0));
+        connectionManager.add(sayerConnection);
 
         when(textMaker.getText(TextName.MissingSayText, locale)).thenReturn(responseMessage);
 
-        command.execute(sayerConnection, "`", target.getName() + " ", configuration);
+        command.execute(sayerConnection, "`", "", configuration);
 
         assertThat(sayerConnection.getOutputQueue(), hasItem(responseMessage));
+    }
+
+    private Connection connection(Player player, GameCharacter character) {
+        return TestObjectBuilder.connection().withPlayer(player).withCharacter(character);
+    }
+
+    private GameCharacter character(String name, long locationId) {
+        return new CharacterBuilder().withName(name).withLocationId(locationId).build();
     }
 
     @Test
     public void messageOnlySpaces() {
         String responseMessage = RandomStringUtils.randomAlphabetic(18);
+        Connection sayerConnection = connection(player, character("", 0));
+        connectionManager.add(sayerConnection);
 
         when(textMaker.getText(TextName.MissingSayText, locale)).thenReturn(responseMessage);
 
-        command.execute(sayerConnection, "`", target.getName() + "       ", configuration);
+        command.execute(sayerConnection, "`", RandomStringUtils.randomAlphabetic(7) + "       ", configuration);
 
         assertThat(sayerConnection.getOutputQueue(), hasItem(responseMessage));
     }
 
     @Test
     public void messageSeenByInSpaceCharacter() {
-        Connection inRoomConnection = new Connection(configuration).withCharacter(new CharacterBuilder().withLocationId(sayer.getLocationId()).build());
-        connections.add(inRoomConnection);
-
         String message = RandomStringUtils.randomAlphabetic(17);
+        String characterName = RandomStringUtils.randomAlphabetic(7);
+        String targetName = RandomStringUtils.randomAlphabetic(8);
+        long locationId = RandomUtils.nextLong(100, 200);
 
-        command.execute(sayerConnection, "`", target.getName() + " " + message, configuration);
-        String expected = sayer.getName() + " says to " + target.getName() + ", \"" + message + "\"\\r\\n";
+        Connection sayerConnection = connection(null, character(characterName, locationId));
+        Connection inRoomConnection = TestObjectBuilder.connection().withCharacter(character("", locationId));
+        connectionManager.add(sayerConnection);
+        connectionManager.add(connection(null, character(targetName, locationId)));
+        connectionManager.add(inRoomConnection);
+
+        command.execute(sayerConnection, "`", targetName + " " + message, configuration);
+
+        String expected = characterName + " says to " + targetName + ", \"" + message + "\"" + GlobalConstants.NEW_LINE;
         assertThat(inRoomConnection.getOutputQueue(), hasItem(expected));
     }
 
     @Test
     public void messageNotSeenByOutOfSpaceCharacter() {
-        Connection outOfRoomConnection = new Connection(configuration)
-                .withCharacter(new CharacterBuilder().withLocationId(RandomUtils.nextLong(500, 600)).build());
-        connections.add(outOfRoomConnection);
-
         String message = RandomStringUtils.randomAlphabetic(17);
+        String characterName = RandomStringUtils.randomAlphabetic(7);
+        String targetName = RandomStringUtils.randomAlphabetic(8);
+        long locationId = RandomUtils.nextLong(100, 200);
 
-        command.execute(sayerConnection, "`", target.getName() + " " + message, configuration);
+        Connection sayerConnection = connection(null, character(characterName, locationId));
+        Connection outOfRoomConnection = TestObjectBuilder.connection().withCharacter(character("", RandomUtils.nextLong(500, 600)));
+        connectionManager.add(sayerConnection);
+        connectionManager.add(connection(null, character(targetName, locationId)));
+        connectionManager.add(outOfRoomConnection);
+
+        command.execute(sayerConnection, "`", targetName + " " + message, configuration);
 
         assertThat(outOfRoomConnection.getOutputQueue().size(), equalTo(0));
     }
