74638cb29040ade6265bec93b7c3f7a7704aeff7
Iain E. Davis <iain@ruhlendavis.org>
2016-01-24 07:43:14 -0600

    Make CharacterNamePrompt injected.

--- src/main/java/org/mumue/mumue/connection/states/CharacterNamePrompt.java
+++ src/main/java/org/mumue/mumue/connection/states/CharacterNamePrompt.java
@@ -2,19 +2,18 @@ package org.mumue.mumue.connection.states;
 
 import javax.inject.Inject;
 
-import com.google.inject.Injector;
 import org.mumue.mumue.configuration.ApplicationConfiguration;
 import org.mumue.mumue.connection.Connection;
 import org.mumue.mumue.text.TextMaker;
 import org.mumue.mumue.text.TextName;
 
 public class CharacterNamePrompt implements ConnectionState {
-    private final Injector injector;
+    private final WaitForCharacterName waitForCharacterName;
     private final TextMaker textMaker;
 
     @Inject
-    public CharacterNamePrompt(Injector injector, TextMaker textMaker) {
-        this.injector = injector;
+    public CharacterNamePrompt(WaitForCharacterName waitForCharacterName, TextMaker textMaker) {
+        this.waitForCharacterName = waitForCharacterName;
         this.textMaker = textMaker;
     }
 
@@ -22,6 +21,6 @@ public class CharacterNamePrompt implements ConnectionState {
     public ConnectionState execute(Connection connection, ApplicationConfiguration configuration) {
         String text = textMaker.getText(TextName.CharacterNamePrompt, connection.getLocale());
         connection.getOutputQueue().push(text);
-        return injector.getInstance(WaitForCharacterName.class);
+        return waitForCharacterName;
     }
 }
