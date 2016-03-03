f58f731374abaa80114b8935168c2141ba117602
Iain E. Davis <iain@ruhlendavis.org>
2016-03-03 08:04:14 -0600

    Write version to test Fuzzball database.

--- src/test/java/org/mumue/mumue/databaseimporter/testapi/DatabaseLinesBuilder.java
+++ src/test/java/org/mumue/mumue/databaseimporter/testapi/DatabaseLinesBuilder.java
@@ -6,10 +6,11 @@ import java.util.List;
 public class DatabaseLinesBuilder implements LineBuilder {
     private String format = "***Foxen5 TinyMUCK DUMP Format***";
     private int itemCount;
+    private String version = "1";
 
     @Override
     public List<String> getLines() {
-        return Arrays.asList(format, String.valueOf(itemCount));
+        return Arrays.asList(format, String.valueOf(itemCount), version);
     }
 
     public DatabaseLinesBuilder withFormat(String format) {
@@ -19,6 +20,11 @@ public class DatabaseLinesBuilder implements LineBuilder {
 
     public DatabaseLinesBuilder withDatabaseItemCount(int itemCount) {
         this.itemCount = itemCount;
+        return this;
+    }
+
+    public DatabaseLinesBuilder withFormatVersion(String version) {
+        this.version = version;
         return this;
     }
 }
