f46f3ccfa69ec6c83104ed4a5d12afb9a864fc98
Iain E. Davis <iain@iaindavis.name>
2025-05-04 17:13:02 -0600

    Fix issues with line loading. Primarily exception handling and file closing

--- src/main/java/org/mumue/mumue/databaseimporter/LineLoader.java
+++ src/main/java/org/mumue/mumue/databaseimporter/LineLoader.java
@@ -2,6 +2,9 @@ package org.mumue.mumue.databaseimporter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,14 +12,15 @@ import java.util.Scanner;
 class LineLoader {
     public List<String> loadFrom(File file) {
         List<String> lines = new ArrayList<>();
-        try {
-            Scanner scanner = new Scanner(file, "US-ASCII");
+        try (Scanner scanner = new Scanner(file, StandardCharsets.US_ASCII)) {
             scanner.useDelimiter("\\x0A");
             while (scanner.hasNext()) {
                 lines.add(scanner.next());
             }
         } catch (FileNotFoundException exception) {
             throw new RuntimeException(exception);
+        } catch (IOException exception) {
+            throw new UncheckedIOException(exception);
         }
         return lines;
     }
