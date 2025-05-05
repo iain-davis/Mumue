1cf86075cbd815999d60dba95e81fd8813bcc566
Iain E. Davis <iain@iaindavis.name>
2025-05-04 20:17:24 -0600

    Migrate to Jupiter

--- src/test/java/org/mumue/mumue/threading/ThreadFactoryTest.java
+++ src/test/java/org/mumue/mumue/threading/ThreadFactoryTest.java
@@ -1,45 +1,45 @@
 package org.mumue.mumue.threading;
 
-import static org.hamcrest.Matchers.startsWith;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class ThreadFactoryTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.startsWith;
+
+class ThreadFactoryTest {
     private final ThreadFactory factory = new ThreadFactory();
 
     @Test
-    public void createReturnsThread() {
+    void createReturnsThread() {
         Thread thread = factory.create(new MockRunnable());
-        assertNotNull(thread);
+        assertThat(thread, notNullValue());
     }
 
     @Test
-    public void createSetsName() {
+    void createSetsName() {
         String name = RandomStringUtils.insecure().nextAlphabetic(13);
         Thread thread = factory.create(new MockRunnable(), name);
-        assertNotNull(thread);
-        assertEquals(name, thread.getName());
+
+        assertThat(thread.getName(), equalTo(name));
     }
 
     @Test
-    public void createHandlesNullName() {
+    void createHandlesNullName() {
         Thread thread = factory.create(new MockRunnable(), null);
-        assertNotNull(thread);
+
         assertThat(thread.getName(), startsWith("Thread-"));
     }
 
     @Test
-    public void createHandlesBlankName() {
+    void createHandlesBlankName() {
         Thread thread = factory.create(new MockRunnable(), "");
-        assertNotNull(thread);
+
         assertThat(thread.getName(), startsWith("Thread-"));
     }
 
-    private class MockRunnable implements Runnable {
+    private static class MockRunnable implements Runnable {
         @Override
         public void run() {
         }
